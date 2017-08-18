293487a0b5473e3eaecc4936292c20af8a60db2b
dbconrado <dbconrado@gmail.com>
2017-08-18 10:58:37 -0300

    nova funcionalidade: cadastrar um livro

---
+++ site/cadastrar_livro.php
@@ -0,0 +1,43 @@
+<?php
+
+/* este script cadastra um livro no banco de dados.
+ *
+ * INSTRUÇÕES:
+ *
+ * envie os seguintes dados via POST:
+ *
+ * titulo - o título do livro a ser cadastrado
+ *
+ * */
+
+/* PRIMEIRO PASSO: receber os dados */
+if (empty($_POST['titulo'])) {
+   die("Uso incorreto deste script.");
+}
+
+$titulo = $_POST['titulo'];
+
+/* SEGUNDO PASSO: montar o comando SQL */
+
+$sql = "INSERT INTO Livro (titulo) VALUES ('${titulo}')";
+
+/* TERCEIRO PASSO: conectar-se ao banco de dados */
+
+$conexao = mysqli_connect("localhost", "root", "root", "BiblioWeb");
+if (!$conexao) {
+   die("Não consegui me conectar ao banco. Mensagem de erro: " . mysqli_connect_error());
+}
+
+/* QUARTO PASSO: executar o comando SQL */
+
+$sucesso = mysqli_query($conexao, $sql);
+if (!$sucesso) {
+   echo "Não consegui realizar o cadastro.";
+} else {
+   echo "Cadastrado com sucesso!";
+}
+
+/* QUINTO E ÚLTIMO PASSO: fechar a conexão */
+mysqli_close($conexao);
+
+?>
